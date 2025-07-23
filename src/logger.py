202de5b4ebbc4b9bca1f66641caa8e07e669c9de
Chandan Nafria <chandannafria@gmail.com>
2025-07-23 18:05:15 +0530

    logger update

--- src/logger.py
+++ src/logger.py
@@ -1,19 +1,41 @@
 import logging
 import os
 from datetime import datetime
+import logging
+import os
+from datetime import datetime
 
-# Log file name with timestamp
-log_file = f"log_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
-log_path = os.path.join("logs", log_file)
-os.makedirs("logs", exist_ok=True)
+# Step 1: Create a logs directory
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
 
-log_file_path  = os.path.join(log_path , log_file)
-# Configure logging
+# Step 2: Create log file name (just the file, not a folder)
+LOG_FILE = f"log_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+
+# Step 3: Combine to form full path
+LOG_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Step 4: Setup logger
 logging.basicConfig(
-    filename=log_file_path,
+    filename=LOG_PATH,
     level=logging.INFO,
-    format='[%(asctime)s] %(levelname)s: %(message)s'
+    format='[%(asctime)s] %(levelname)s - %(message)s'
 )
 
-if __name__ =="__main__":
-    logging.info("logging has statred")
+logger = logging.getLogger(__name__)
+
+
+# # Log file name with timestamp
+# log_file = f"log_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+# log_path = os.path.join(os.getcwd(),"logs", log_file)
+# os.makedirs(log_path, exist_ok=True)
+
+# log_file_path  = os.path.join(log_path , log_file)
+# # Configure logging
+# logging.basicConfig(
+#     filename=log_file_path,
+#     level=logging.INFO,
+#     format='[%(asctime)s] %(levelname)s: %(message)s'
+# )
+
+# # logger = logging.getLogger(__name__)
